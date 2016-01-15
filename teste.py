16f6d288632ef8d9618d0877645fec5431b1e5d8
lucasjovencio <lucasjovencio@outlook.com.br>
2016-01-15 13:00:27 -0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,8 @@
-main():
-  print("teste")
+def f_print (n):
+    print ("Bem vindo "+n)
+   
+def main():
+    nome = str(input("Digite o seu nome: "))
+    print(f_print(nome))
 if __name__ == '__main__':
-	main()
+    main()
